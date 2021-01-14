6266422f2461d15f96615e2f67b5944ae79b7553
Calterat <Calterat@gmail.com>
2021-01-14 02:07:54 -0600

    minor file relocations

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
