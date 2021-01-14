389c0f8d1199b9348f71d67b878c91b102a226fc
Calterat <Calterat@gmail.com>
2021-01-14 02:45:00 -0600

    working on vue routers

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,24 @@
+<!--
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
+-->
+<template>
+  <div id="vueApp">
+    <router-view />
+  </div>
+</template>
+
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'vueApp',
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/css/bootstrap.css"
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,31 @@
-import { createApp } from 'vue'
-import App2 from './App2'
-import Notelist from './Notelist'
+// import { createApp } from 'vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-createApp(App2).mount('#app')
-createApp(Notelist).mount('#notelist')
+import RetrieveUser from './components/User.vue';
+import RetrieveNote from './components/Note.vue';
+
+import App from './App'
+
+// createApp(App).mount('#app')
+
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+const routes = [
+  {
+    name: 'RetrieveUser',
+    path: 'api/user',
+    component: RetrieveUser
+  },
+  {
+    name: 'RetrieveNote',
+    path: 'api/note',
+    component: RetrieveNote
+  }
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes });
+
+new Vue({ router, render: app => app(App) }).mount('#app');
